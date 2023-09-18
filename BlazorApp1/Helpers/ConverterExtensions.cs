a32ce2b7bf4a54ce0e9cf14a3726647ed8520d01
solomkin <solomkin@skbkontur.ru>
2023-09-18 19:30:16 +0500

    Added ReferenceBuilder

---
+++ BlazorApp1/Helpers/ConverterExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorApp1.Helpers;
+
+public static class ConverterExtensions
+{
+    public static string ConvertPathSeparator(this string input) => input.Replace("\\", "/");
+    public static string ConvertColonToGrid(this string input) => input.Replace(":", "#");
+}
